62cfa035f839f45bdbf639b5e7a1066290a9cf87
Bryan Steven Manobanda <bryan.manobanda@epn.edu.ec>
2024-01-13 23:19:54 -0500

    Integración con SQLite

--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_bsmc/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_bsmc/MainActivity.kt
@@ -62,6 +62,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        EBaseDeDatos.tablaEntrenador = ESqliteHelperEntrenador(this)
         val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
         botonCicloVida
             .setOnClickListener {
@@ -93,6 +95,12 @@ class MainActivity : AppCompatActivity() {
                 CIntentExplicitoParametros::class.java
             )
         }
+
+        val botonSqlite = findViewById<Button>(R.id.btn_sqlite)
+        botonSqlite
+            .setOnClickListener {
+                irActividad(ECrudEntrenador::class.java)
+            }
     }
 
     fun abrirActividadParametros(
